627f6b2e6bae236e62d9672b6d3f1a59f6590617
taeheeL <t9h9@naver.com>
2023-04-14 23:39:35 +0900

    AAC ViewModel

---
+++ app/src/main/java/org/android/go/sopt/HomeViewModel.kt
@@ -0,0 +1,66 @@
+package org.android.go.sopt
+
+import androidx.lifecycle.ViewModel
+import org.android.go.sopt.model.Animal
+
+class HomeViewModel : ViewModel() {
+
+    val mockAnimalList = listOf<Animal>(
+
+        Animal(
+            image = R.drawable.ic_tibet,
+            animal = "티뱃 여우",
+            species = "포유류"
+        ),
+        Animal(
+            image = R.drawable.ic_seal_1,
+            animal = "담배 피는 물개",
+            species = "포유류"
+        ),
+        Animal(
+            image = R.drawable.ic_seal_2,
+            animal = "파란 물개",
+            species = "포유류"
+        ),
+        Animal(
+            image = R.drawable.ic_owl,
+            animal = "부엉이",
+            species = "조류"
+        ),
+        Animal(
+            image = R.drawable.ic_dog,
+            animal = "웃는 개",
+            species = "포유류"
+        ),
+        Animal(
+            image = R.drawable.ic_kjs,
+            animal = "지친 김준서",
+            species = "파충류"
+        ),
+        Animal(
+            image = R.drawable.ic_iron_man,
+            animal = "아이언맨",
+            species = "포유류"
+        ),
+        Animal(
+            image = R.drawable.ic_toruk,
+            animal = "토루크",
+            species = "파충류"
+        ),
+        Animal(
+            image = R.drawable.ic_tibet,
+            animal = "티뱃 여우",
+            species = "포유류"
+        ),
+        Animal(
+            image = R.drawable.ic_seal_1,
+            animal = "담배 피는 물개",
+            species = "포유류"
+        ),
+        Animal(
+            image = R.drawable.ic_seal_2,
+            animal = "파란 물개",
+            species = "포유류"
+        )
+    )
+}
