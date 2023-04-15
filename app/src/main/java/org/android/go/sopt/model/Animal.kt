68def243ba41785e30c2ce86d191fe1b3be9e995
taeheeL <t9h9@naver.com>
2023-04-16 00:54:18 +0900

    adjust Animal Data class

--- app/src/main/java/org/android/go/sopt/model/Animal.kt
+++ app/src/main/java/org/android/go/sopt/model/Animal.kt
@@ -3,6 +3,7 @@ package org.android.go.sopt.model
 import androidx.annotation.DrawableRes
 
 data class Animal(
+    val id:Long,
     @DrawableRes val image: Int,
     val animal : String,
     val species: String
