38aed8a60e432f318ac630fc3e562839ac315955
taeheeL <t9h9@naver.com>
2023-04-17 21:50:11 +0900

    Fragment KTX 사용

--- app/src/main/java/org/android/go/sopt/presentation/MyPageActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/MyPageActivity.kt
@@ -1,13 +1,14 @@
 package org.android.go.sopt.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import org.android.go.sopt.R
 import org.android.go.sopt.base.BindingActivity
 import org.android.go.sopt.databinding.ActivityMyPageBinding
 import org.android.go.sopt.presentation.gallery.GalleryFragment
 import org.android.go.sopt.presentation.home.HomeFragment
 import org.android.go.sopt.presentation.search.SearchFragment
+import org.android.go.sopt.util.extension.replace
+import timber.log.Timber
 
 class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,32 +22,18 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
     }
 
     private fun initLayout() {
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.home_container)
-        if (currentFragment == null) {
-            supportFragmentManager.beginTransaction().add(R.id.home_container, HomeFragment())
-                .commit()
-            binding.bnvHome.selectedItemId = R.id.home_menu
-        }
+        changeFragment(R.id.home_menu)
+        binding.bnvHome.selectedItemId = R.id.home_menu
     }
 
     private fun selectFragment() {
-        binding.bnvHome.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home_menu -> {
-                    changeFragment(HomeFragment())
-                }
-                R.id.gallery_menu -> {
-                    changeFragment(GalleryFragment())
-                }
-                R.id.search_menu -> {
-                    changeFragment(SearchFragment())
-                }
-                else -> error("item id: {${it.itemId} is cannot be selected")
+        binding.bnvHome.run {
+            setOnItemSelectedListener {
+                changeFragment(it.itemId)
+                true
             }
-            return@setOnItemSelectedListener true
         }
     }
-
 
     private fun reselectFragment() {
         binding.bnvHome.setOnItemReselectedListener {
@@ -57,12 +44,14 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
         }
     }
 
-
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.home_container, fragment)
-            .commit()
+    private fun changeFragment(menuItemId: Int) = when (menuItemId) {
+        R.id.home_menu -> replace<HomeFragment>(
+            R.id.home_container,
+            HomeFragment::class.java.simpleName
+        )
+        R.id.gallery_menu -> replace<GalleryFragment>(R.id.home_container)
+        R.id.search_menu -> replace<SearchFragment>(R.id.home_container)
+        else -> Timber.e(IllegalArgumentException("Not found menu item id"))
     }
 
 }
