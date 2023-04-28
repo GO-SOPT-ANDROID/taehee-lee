1b6f0dc9680783a64c154da21e833ba9d3390a56
taeheeL <t9h9@naver.com>
2023-04-28 15:28:06 +0900

    #3 다이얼로그 생성 & 로그아웃 버튼에 연결

--- app/src/main/java/org/android/go/sopt/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/mypage/MyPageFragment.kt
@@ -1,20 +1,56 @@
 package org.android.go.sopt.presentation.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import org.android.go.sopt.GoSoptApplication
 import org.android.go.sopt.R
 import org.android.go.sopt.base.BindingFragment
 import org.android.go.sopt.databinding.FragmentMyPageBinding
+import org.android.go.sopt.model.GoSoptSharedPreference
+import org.android.go.sopt.presentation.LoginActivity
+import org.android.go.sopt.util.extension.showSnackbar
+import org.android.go.sopt.util.extension.showToast
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setMyPage()
+        clickLogoutButton()
     }
 
     private fun setMyPage() {
         binding.tvName.text = GoSoptApplication.prefs.getUserInfo()?.name
         binding.tvSpecialty.text = GoSoptApplication.prefs.getUserInfo()?.specialty
+    }
+
+    private fun clickLogoutButton() {
+        binding.btnLogout.setOnClickListener {
+            showLogoutDialog().show()
+        }
+    }
+
+    private fun showLogoutDialog(): AlertDialog.Builder {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("로그아웃 하시겠습니까?")
+            .setCancelable(false)
+            .setNegativeButton(
+                "아니오"
+            ) { _, _ ->
+                showToast(requireContext(), "로그아웃을 취소합니다.")
+            }
+            .setPositiveButton("예") { p0, p1 ->
+                deleteUserData()
+                showSnackbar(binding.root, "로그아웃 하였습니다.")
+                startActivity(Intent(requireContext(), LoginActivity::class.java))
+            }
+            .create()
+        return builder
+
+    }
+
+    private fun deleteUserData() {
+        GoSoptSharedPreference(requireContext()).deleteUserInfo()
     }
 }
