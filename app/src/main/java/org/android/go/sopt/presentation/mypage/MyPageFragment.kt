a152583ea2b3a2b88261d47dd0484aa7a1c17ffe
taeheeL <t9h9@naver.com>
2023-04-28 14:45:15 +0900

    #3 My Page에서 데이터 불러오기

--- app/src/main/java/org/android/go/sopt/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/mypage/MyPageFragment.kt
@@ -2,13 +2,19 @@ package org.android.go.sopt.presentation.mypage
 
 import android.os.Bundle
 import android.view.View
+import org.android.go.sopt.GoSoptApplication
 import org.android.go.sopt.R
 import org.android.go.sopt.base.BindingFragment
 import org.android.go.sopt.databinding.FragmentMyPageBinding
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setMyPage()
+    }
+
+    private fun setMyPage() {
+        binding.tvName.text = GoSoptApplication.prefs.getUserInfo()?.name
+        binding.tvSpecialty.text = GoSoptApplication.prefs.getUserInfo()?.specialty
     }
 }
