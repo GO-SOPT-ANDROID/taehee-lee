f3c3f5f130789c45911baa25bd9c7f14759cdc5c
taeheeL <t9h9@naver.com>
2023-05-04 20:46:04 +0900

    #3 data 전달 값 ViewModel에서 가져오기

--- app/src/main/java/org/android/go/sopt/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/SignUpActivity.kt
@@ -4,16 +4,22 @@ import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
 import org.android.go.sopt.R
 import org.android.go.sopt.base.BindingActivity
 import org.android.go.sopt.databinding.ActivitySignUpBinding
-import org.android.go.sopt.model.UserInfo
+import org.android.go.sopt.domain.model.UserInfo
 import org.android.go.sopt.presentation.login.LoginActivity.Companion.USER_INFO
 
 class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+
+    private val viewModel by viewModels<SignUpViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         clickSignUpCompleteBtn()
         binding.layoutSignUp.setOnClickListener {
@@ -21,21 +27,13 @@ class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_
         }
     }
 
-    private fun checkSignUpValid(): Boolean {
-        return ((binding.etId.text.length in 6..12)
-                && (binding.etPassword.text.length in 8..12)
-                && binding.etName.text.isNotEmpty()
-                && binding.etSpecialty.text.isNotEmpty())
-
-    }
-
     private fun passUserData() {
-        if (checkSignUpValid()) {
+        if (binding.btnSignUpComplete.isEnabled) {
             val userInfo = UserInfo(
-                binding.etId.text.toString(),
-                binding.etPassword.text.toString(),
-                binding.etName.text.toString(),
-                binding.etSpecialty.text.toString()
+                viewModel.id.toString(),
+                viewModel.password.toString(),
+                viewModel.name.toString(),
+                viewModel.specialty.toString()
             )
 
             intent.putExtra(USER_INFO, userInfo)
