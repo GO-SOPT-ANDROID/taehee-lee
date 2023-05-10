b5d5a1ab91aebcc5a8fa71a5c6daa4a0689da6f0
taeheeL <t9h9@naver.com>
2023-05-10 16:06:25 +0900

    #5 Hilt 세팅

--- app/src/main/java/org/android/go/sopt/GoSoptApplication.kt
+++ app/src/main/java/org/android/go/sopt/GoSoptApplication.kt
@@ -1,9 +1,11 @@
 package org.android.go.sopt
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import org.android.go.sopt.data.local.GoSoptSharedPreference
 import timber.log.Timber
 
+@HiltAndroidApp
 class GoSoptApplication: Application() {
     override fun onCreate() {
         prefs = GoSoptSharedPreference(applicationContext)
