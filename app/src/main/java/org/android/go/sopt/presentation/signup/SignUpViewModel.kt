deb05439aec11b071fb7149c9d1a57624e95e1f0
taeheeL <t9h9@naver.com>
2023-05-11 12:44:37 +0900

    #5 이름, 특기 조건 추가 & 코루틴으로 통신

--- app/src/main/java/org/android/go/sopt/presentation/signup/SignUpViewModel.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/SignUpViewModel.kt
@@ -1,20 +1,48 @@
 package org.android.go.sopt.presentation.signup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import org.android.go.sopt.base.ID_PATTERN
 import org.android.go.sopt.base.PW_PATTERN
+import org.android.go.sopt.data.local.GoSoptSharedPreference
+import org.android.go.sopt.domain.repository.AuthRepository
+import org.android.go.sopt.util.Event
+import javax.inject.Inject
 
-class SignUpViewModel : ViewModel() {
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val goSoptSharedPreference: GoSoptSharedPreference,
+    private val authRepository: AuthRepository
+) : ViewModel() {
+
+    private val _isCompletedSignUp = MutableLiveData<Event<Boolean>>()
+    val isCompletedSignUp: LiveData<Event<Boolean>> get() = _isCompletedSignUp
 
     val id = MutableStateFlow("")
     val password = MutableStateFlow("")
     val name = MutableStateFlow("")
     val specialty = MutableStateFlow("")
 
-    val checkSignUpState = combine(id, password) { idValue, passwordValue ->
-        if (idValue.matches(Regex(ID_PATTERN)) && passwordValue.matches(Regex(PW_PATTERN))) "valid" else "invalid"
+    fun postSignUpResult() {
+        viewModelScope.launch {
+            val isSuccessful =
+                authRepository.signUp(id.value, password.value, name.value, specialty.value)
+            _isCompletedSignUp.value = Event(isSuccessful)
+        }
+    }
+
+    val checkSignUpState = combine(
+        id,
+        password,
+        name,
+        specialty
+    ) { idValue, passwordValue, nameValue, specialtyValue ->
+        if (idValue.matches(Regex(ID_PATTERN)) && passwordValue.matches(Regex(PW_PATTERN)) && nameValue.isNotBlank() && specialtyValue.isNotBlank()) "valid" else "invalid"
     }
         .debounce(300L)
         .distinctUntilChanged()
