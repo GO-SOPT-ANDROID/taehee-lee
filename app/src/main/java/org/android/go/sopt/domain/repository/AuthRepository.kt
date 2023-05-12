75a3876af6090e917b713254ece1238f616f1d5a
taeheeL <t9h9@naver.com>
2023-05-12 17:59:15 +0900

    #5 interface에 자동 로그인 함수 추가

--- app/src/main/java/org/android/go/sopt/domain/repository/AuthRepository.kt
+++ app/src/main/java/org/android/go/sopt/domain/repository/AuthRepository.kt
@@ -1,6 +1,10 @@
 package org.android.go.sopt.domain.repository
 
 interface AuthRepository {
+    fun clearPref()
+    fun getAutoMode(): Boolean
+    fun setAutoMode(isAutoMode: Boolean)
+
     suspend fun signUp(id: String, password: String, name: String, skill: String): Boolean
     suspend fun signIn(id: String, password: String): Boolean
 }
