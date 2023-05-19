7d2bc6202f5e23a1e89b356c60f2c9ad5fae2bad
taeheeL <t9h9@soongsil.ac.kr>
2023-05-20 00:36:10 +0900

    #7 반환값 변경

--- app/src/main/java/org/android/go/sopt/data/service/KaKaoService.kt
+++ app/src/main/java/org/android/go/sopt/data/service/KaKaoService.kt
@@ -1,16 +1,16 @@
 package org.android.go.sopt.data.service
 
 import org.android.go.sopt.BuildConfig.KAKAO_REST_API_KEY
-import org.android.go.sopt.domain.model.SearchResponse
-import retrofit2.Response
+import org.android.go.sopt.data.model.KakaoSearchResponseDto
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
-interface KaKaoSearchService {
+interface KaKaoService {
     @GET("/v2/search/web")
-    suspend fun getSearchWeb(
-        @Header("Authorization") key: String = KAKAO_REST_API_KEY,
+    fun getSearchWeb(
+        @Header("Authorization") key: String = "KakaoAK $KAKAO_REST_API_KEY",
         @Query("query") keyword: String
-    ): Response<SearchResponse>
+    ): Call<KakaoSearchResponseDto>
 }
